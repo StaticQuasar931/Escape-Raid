b410a7b60a5e4869a3ee3937cd2da50d9e7da250
StaticQuasar931 <StaticQuasar931@gmail.com>
2026-04-15 15:49:52 -0700

    4

--- gmsoftsdk_v5.js
+++ gmsoftsdk_v5.js
@@ -1,14 +1,33 @@
 (function() {
-  const gamesdkUrlPattern = /api\.cdnwave\.com\/sdkdom\/gamesdk/i;
+  const sdkUrlMatchers = [
+    /api\.cdnwave\.com\/sdkdom\/gamesdk/i,
+    /api\.azgame\.io\/sdk\/gmadsv1/i,
+    /games\.yoplay\.io\/sdk\/gmadsv1/i
+  ];
+
+  function noop() {}
+
+  function matchesSdkUrl(url) {
+    return typeof url === "string" && sdkUrlMatchers.some(function(pattern) {
+      return pattern.test(url);
+    });
+  }
+
+  function safeJson(value) {
+    try {
+      return JSON.stringify(value);
+    } catch (error) {
+      return "{}";
+    }
+  }
 
   function installFirebaseStubs() {
-    function noop() {}
     window.firebaseLogEvent = window.firebaseLogEvent || noop;
     window.firebaseLogEventParameter = window.firebaseLogEventParameter || noop;
     window.firebaseSetUserProperties = window.firebaseSetUserProperties || noop;
   }
 
-  function installAdStubs() {
+  function installAdsStubs() {
     window.adsbygoogle = window.adsbygoogle || [];
     window.adConfig = window.adConfig || function(options) {
       if (options && typeof options.onReady === "function") {
@@ -26,15 +45,15 @@
         options.adBreakDone();
       }
     };
-    window._showRewardAdFn = window._showRewardAdFn || function(callback) {
-      if (typeof callback === "function") {
-        callback();
-      }
+    window._showRewardAdFn = window._showRewardAdFn || null;
+    window.afg = window.afg || {
+      ready: true,
+      onBeforeAd: noop,
+      onAfterAd: noop
     };
   }
 
-  function installSdkStubs(gameInfoParam, adInfoParam) {
-    function noop() {}
+  function installSdkGlobals(gameInfoParam, adInfoParam) {
     function runCallback(callback) {
       if (typeof callback === "function") {
         callback();
@@ -70,135 +89,196 @@
     };
   }
 
-  function installNetworkShim(makeBody) {
-    if (!window.__gmsoftEscapeRaidNetworkInstalled) {
-      window.__gmsoftEscapeRaidNetworkInstalled = true;
+  function installNetworkShim(getBody) {
+    if (window.__gmsoftEscapeRaidNetworkInstalled) {
+      return;
+    }
+    window.__gmsoftEscapeRaidNetworkInstalled = true;
 
-      const nativeFetch = window.fetch ? window.fetch.bind(window) : null;
-      if (nativeFetch) {
-        window.fetch = function(resource, init) {
-          const url = typeof resource === "string" ? resource : resource && resource.url;
-          if (typeof url === "string" && gamesdkUrlPattern.test(url)) {
-            return Promise.resolve(new Response(makeBody(), {
-              status: 200,
-              headers: {
-                "Content-Type": "application/json"
-              }
-            }));
-          }
-          return nativeFetch(resource, init);
-        };
-      }
-
-      const nativeOpen = XMLHttpRequest.prototype.open;
-      const nativeSend = XMLHttpRequest.prototype.send;
-      const nativeSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
-      const nativeGetResponseHeader = XMLHttpRequest.prototype.getResponseHeader;
-      const nativeGetAllResponseHeaders = XMLHttpRequest.prototype.getAllResponseHeaders;
-
-      XMLHttpRequest.prototype.open = function(method, url) {
-        this.__gmsoftSdkIntercept = typeof url === "string" && gamesdkUrlPattern.test(url);
-        if (!this.__gmsoftSdkIntercept) {
-          return nativeOpen.apply(this, arguments);
+    const nativeFetch = window.fetch ? window.fetch.bind(window) : null;
+    if (nativeFetch) {
+      window.fetch = function(resource, init) {
+        const url = typeof resource === "string" ? resource : resource && resource.url;
+        if (matchesSdkUrl(url)) {
+          return Promise.resolve(new Response(getBody(), {
+            status: 200,
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }));
         }
-        this.readyState = 1;
-        if (typeof this.onreadystatechange === "function") {
-          this.onreadystatechange();
-        }
-      };
-
-      XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
-        if (!this.__gmsoftSdkIntercept) {
-          return nativeSetRequestHeader.call(this, name, value);
-        }
-      };
-
-      XMLHttpRequest.prototype.send = function() {
-        if (!this.__gmsoftSdkIntercept) {
-          return nativeSend.apply(this, arguments);
-        }
-
-        const payload = makeBody();
-        Object.defineProperty(this, "readyState", { configurable: true, value: 4 });
-        Object.defineProperty(this, "status", { configurable: true, value: 200 });
-        Object.defineProperty(this, "statusText", { configurable: true, value: "OK" });
-        Object.defineProperty(this, "responseText", { configurable: true, value: payload });
-        Object.defineProperty(this, "response", { configurable: true, value: payload });
-
-        if (typeof this.onreadystatechange === "function") {
-          this.onreadystatechange();
-        }
-        if (typeof this.onload === "function") {
-          this.onload();
-        }
-        if (typeof this.onloadend === "function") {
-          this.onloadend();
-        }
-      };
-
-      XMLHttpRequest.prototype.getResponseHeader = function(name) {
-        if (this.__gmsoftSdkIntercept) {
-          return name && name.toLowerCase() === "content-type" ? "application/json" : null;
-        }
-        return nativeGetResponseHeader.call(this, name);
-      };
-
-      XMLHttpRequest.prototype.getAllResponseHeaders = function() {
-        if (this.__gmsoftSdkIntercept) {
-          return "content-type: application/json\r\n";
-        }
-        return nativeGetAllResponseHeaders.call(this);
+        return nativeFetch(resource, init);
       };
     }
+
+    const nativeOpen = XMLHttpRequest.prototype.open;
+    const nativeSend = XMLHttpRequest.prototype.send;
+    const nativeSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+    const nativeGetResponseHeader = XMLHttpRequest.prototype.getResponseHeader;
+    const nativeGetAllResponseHeaders = XMLHttpRequest.prototype.getAllResponseHeaders;
+
+    XMLHttpRequest.prototype.open = function(method, url) {
+      this.__gmsoftSdkIntercept = matchesSdkUrl(typeof url === "string" ? url : "");
+      if (!this.__gmsoftSdkIntercept) {
+        return nativeOpen.apply(this, arguments);
+      }
+      this.readyState = 1;
+      if (typeof this.onreadystatechange === "function") {
+        this.onreadystatechange();
+      }
+    };
+
+    XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
+      if (!this.__gmsoftSdkIntercept) {
+        return nativeSetRequestHeader.call(this, name, value);
+      }
+    };
+
+    XMLHttpRequest.prototype.send = function() {
+      if (!this.__gmsoftSdkIntercept) {
+        return nativeSend.apply(this, arguments);
+      }
+
+      const payload = getBody();
+      Object.defineProperty(this, "readyState", { configurable: true, value: 4 });
+      Object.defineProperty(this, "status", { configurable: true, value: 200 });
+      Object.defineProperty(this, "statusText", { configurable: true, value: "OK" });
+      Object.defineProperty(this, "responseText", { configurable: true, value: payload });
+      Object.defineProperty(this, "response", { configurable: true, value: payload });
+
+      if (typeof this.onreadystatechange === "function") {
+        this.onreadystatechange();
+      }
+      if (typeof this.onload === "function") {
+        this.onload();
+      }
+      if (typeof this.onloadend === "function") {
+        this.onloadend();
+      }
+    };
+
+    XMLHttpRequest.prototype.getResponseHeader = function(name) {
+      if (this.__gmsoftSdkIntercept) {
+        return name && name.toLowerCase() === "content-type" ? "application/json" : null;
+      }
+      return nativeGetResponseHeader.call(this, name);
+    };
+
+    XMLHttpRequest.prototype.getAllResponseHeaders = function() {
+      if (this.__gmsoftSdkIntercept) {
+        return "content-type: application/json\r\n";
+      }
+      return nativeGetAllResponseHeaders.call(this);
+    };
   }
+
+  function httpGet(url) {
+    if (matchesSdkUrl(url)) {
+      return safeJson(window.__gmsoftEscapeRaidResponseBody || {});
+    }
+    const request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send("");
+    return request.responseText;
+  }
+
+  function isDiffHost() {
+    return false;
+  }
+
+  function dispatchReady() {
+    document.dispatchEvent(new CustomEvent("gmsoftSdkReady"));
+    window.dispatchEvent(new Event("gmsoftSdkReady"));
+  }
+
+  function applyResponse(responseBody, signedToken, gameInfoParam, adInfoParam) {
+    const regisinfo = responseBody.regisinfo || {};
+    const gameinfo = responseBody.gameinfo || {};
+    const promotion = gameinfo.promotion || {};
+    const adsinfo = responseBody.adsinfo || {};
+    const signed = regisinfo.signed || signedToken;
+    const body = safeJson(responseBody);
+
+    window.__gmsoftEscapeRaidResponseBody = responseBody;
+    window.GMSOFT_SDKTYPE = "gm";
+    window.sdkType = "gm";
+    window.GMSOFT_SIGNED = signed;
+    window.regisinfo = regisinfo;
+    window.gameinfo = gameinfo;
+    window.adsinfo = adsinfo;
+    window.GMSOFT_GAME_INFO = {
+      sdktype: window.GMSOFT_SDKTYPE,
+      more_games_url: gameinfo.moregames_url || "",
+      promotion: promotion
+    };
+    window.GMSOFT_ADS_INFO = {
+      enable: adsinfo.enable || "no",
+      sdk_type: adsinfo.sdk_type || "gm",
+      time_show_inter: Number(adsinfo.time_show_inter || 60),
+      time_show_reward: Number(adsinfo.time_show_reward || 60),
+      pubid: adsinfo.pubid || "",
+      reward: adsinfo.reward || "no",
+      gd_game_key: "",
+      wgLibrary: adsinfo.wgLibrary || "",
+      wgConf: adsinfo.wgConf || "",
+      enable_reward: adsinfo.enable_reward || "no",
+      enable_interstitial: adsinfo.enable_interstitial || "no",
+      enable_preroll: adsinfo.enable_preroll || "no"
+    };
+
+    try {
+      localStorage.setItem("gmsdksigndomain", signed);
+      localStorage.setItem("gm_game_info", body);
+      localStorage.setItem("gameInfoResponse", body);
+      localStorage.setItem("gm_game_info_param", gameInfoParam);
+      localStorage.setItem("gm_ad_info_param", adInfoParam);
+    } catch (error) {}
+  }
+
+  function buildVerifyUrl(urlRequest, gameId) {
+    const hostName = location.hostname || "localhost";
+    const timeSlice = Math.floor(Date.now() / 30000);
+    const params = "d=" + hostName +
+      "&gid=" + (gameId || "") +
+      "&hn=" + hostName +
+      "&pn=" + location.pathname +
+      "&ts=" + timeSlice +
+      "&ie=no";
+    return urlRequest + "?params=" + btoa(params);
+  }
+
+  window.httpGet = window.httpGet || httpGet;
+  window.is_diff_host = window.is_diff_host || isDiffHost;
+  window.isDiffHost = window.isDiffHost || isDiffHost;
 
   window.__gmsoftEscapeRaid = {
     init: function(options) {
-      const body = JSON.stringify(options.responseBody);
       const signedToken = options.signedDomainToken;
       const gameInfoParam = options.gameInfoParam;
       const adInfoParam = options.adInfoParam;
+      const urlRequest = options.urlRequest || "https://api.azgame.io/sdk/gmadsv1";
 
       installFirebaseStubs();
-      installAdStubs();
-      installSdkStubs(gameInfoParam, adInfoParam);
+      installAdsStubs();
+      installSdkGlobals(gameInfoParam, adInfoParam);
       installNetworkShim(function() {
-        return JSON.stringify(window.__gmsoftEscapeRaidResponseBody || options.responseBody);
+        return safeJson(window.__gmsoftEscapeRaidResponseBody || options.responseBody);
       });
 
-      window.__gmsoftEscapeRaidResponseBody = options.responseBody;
+      window.GMSOFT_OPTIONS = options.config || {};
       window.GMSOFT_SDKTYPE = "gm";
       window.sdkType = "gm";
-      window.GMSOFT_SIGNED = signedToken;
-      window.gameinfo = options.responseBody.gameinfo;
-      window.adsinfo = options.responseBody.adsinfo;
-      window.regisinfo = options.responseBody.regisinfo;
-      window.GMSOFT_GAME_INFO = {
-        sdktype: "gm",
-        more_games_url: options.responseBody.gameinfo.moregames_url || "",
-        promotion: options.responseBody.gameinfo.promotion || {}
-      };
-      window.GMSOFT_ADS_INFO = {
-        enable: options.responseBody.adsinfo.enable || "no",
-        sdk_type: "gm",
-        time_show_inter: Number(options.responseBody.adsinfo.time_show_inter || 0),
-        time_show_reward: Number(options.responseBody.adsinfo.time_show_reward || 0),
-        reward: options.responseBody.adsinfo.enable_reward || "no",
-        enable_reward: options.responseBody.adsinfo.enable_reward || "no",
-        enable_interstitial: options.responseBody.adsinfo.enable_interstitial || "no",
-        enable_preroll: options.responseBody.adsinfo.enable_preroll || "no"
-      };
 
       try {
-        localStorage.setItem("gmsdksigndomain", signedToken);
-        localStorage.setItem("gm_game_info", body);
-        localStorage.setItem("gameInfoResponse", body);
-        localStorage.setItem("gm_game_info_param", gameInfoParam);
-        localStorage.setItem("gm_ad_info_param", adInfoParam);
-      } catch (error) {}
+        const verifyUrl = buildVerifyUrl(urlRequest, window.GMSOFT_OPTIONS.gameId || "");
+        const bodyText = httpGet(verifyUrl);
+        const responseBody = JSON.parse(bodyText || "{}");
+        applyResponse(responseBody, signedToken, gameInfoParam, adInfoParam);
+      } catch (error) {
+        applyResponse(options.responseBody || {}, signedToken, gameInfoParam, adInfoParam);
+      }
 
-      document.dispatchEvent(new CustomEvent("gmsoftSdkReady"));
-      window.dispatchEvent(new Event("gmsoftSdkReady"));
+      dispatchReady();
     }
   };
 })();
